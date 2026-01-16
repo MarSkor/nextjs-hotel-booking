2c8d96edb2520477f758b5c7a7fe4d2c3be994e7
Martine <maskor_@outlook.com>
2026-01-16 19:42:19 +0100

    drizzle

---
+++ drizzle/0002_fearless_microchip.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "reviews" DROP CONSTRAINT "reviews_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
